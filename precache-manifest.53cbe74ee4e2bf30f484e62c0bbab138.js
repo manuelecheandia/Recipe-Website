418b37ea7a2a5f27c30665a46eb91045543fecfd
Manuel Echeandia <manuel.ep.10@gmail.com>
2024-09-18 22:59:20 -0300

    Updates

---
+++ precache-manifest.53cbe74ee4e2bf30f484e62c0bbab138.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273a942ccfe0cd1d236b44f018273843",
+    "url": "/recipe-website/index.html"
+  },
+  {
+    "revision": "14fddcb9602e4e1f2628",
+    "url": "/recipe-website/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "7e2954ff39c2ae9d2b7d",
+    "url": "/recipe-website/static/js/2.efe87899.chunk.js"
+  },
+  {
+    "revision": "d2c463aa78614e3c99fe124ad42137d1",
+    "url": "/recipe-website/static/js/2.efe87899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d06153fed39fe8009d",
+    "url": "/recipe-website/static/js/3.2fc5696b.chunk.js"
+  },
+  {
+    "revision": "14fddcb9602e4e1f2628",
+    "url": "/recipe-website/static/js/main.dd53a4bf.chunk.js"
+  },
+  {
+    "revision": "37ae5535fbfcfb8e77ce",
+    "url": "/recipe-website/static/js/runtime-main.abd4c58f.js"
+  }
+]);
